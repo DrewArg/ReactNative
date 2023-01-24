c477f5c8a396bea7f6b8a881c1e117d894a909d3
DrewArg <andresfabbiano5@gmail.com>
2023-01-23 21:55:48 -0300

    added src folder

---
+++ desafios/segundo-desafio/any-game-companion/src/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Hola manola</Text>
+        </View>
+    );
+};
+
+export default App;
